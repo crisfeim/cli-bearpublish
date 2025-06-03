953c48ca545e28bfff4c7d1df58ccf3ae84bab67
Cristian Rojas <cristian.rojas@live.fr>
2025-06-03 17:57:59 +0200

    Add original generation logic to CLI

--- Sources/BearPublisherCLI/CLI.swift
+++ Sources/BearPublisherCLI/CLI.swift
@@ -5,5 +5,18 @@ import Foundation
 
 @main
 struct CLI: ParsableCommand {
+    
+    @Option var destination: String
     mutating func run() throws {}
+    
+    func execute() throws {
+        try copyDatabase()
+       
+        let ssg = SSG()
+        let dbPath = "/Users/\(NSUserName())/Library/Containers/lat.cristian.Renard/Data/Applications/database.sqlite"
+        try ssg.setupDb(location: dbPath)
+        
+        let destinationURL = URL(fileURLWithPath: destination)
+        try? ssg.build(on: destinationURL) {}
+    }
 }
