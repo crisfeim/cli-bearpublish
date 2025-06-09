7b69abd61e6a42f5f15961870debf32a828d3839
Cristian Rojas <cristian.rojas@live.fr>
2025-06-09 15:12:01 +0200

    Extract htmx helpers to their own file

--- Sources/BearPublisherWeb/Extensions/Plot/Plot.Customizations.swift
+++ Sources/BearPublisherWeb/Extensions/Plot/Plot.Customizations.swift
@@ -12,29 +12,9 @@ extension Component {
     func tabIndex(_ value: Int) -> Component {
         attribute(named: "tabindex", value: value.description)
     }
-    func hxGet(_ route: String) -> Component {
-        attribute(named: "hx-get", value: route)
-    }
-    
-    func hxTarget(_ target: String) -> Component {
-        attribute(named: "hx-target", value: target)
-    }
-    
-    func hxPushUrl(_ route: String) -> Component {
-        attribute(named: "hx-push-url", value: route)
-    }
-    
-    /// Specifies the element that will have the htmx-request class added to it for the duration of the request.
-    func hxIndicator(_ indicator: Indicator) -> Component {
-        attribute(named: "hx-indicator", value: indicator.value)
-    }
     
     func hyperScript(_ script: String) -> Component {
         attribute(named: "_", value: script)
-    }
-    
-    func hxSwap(_ value: String) -> Component {
-        attribute(named: "hx-swap", value: value)
     }
     
     func spacing(_ spacing: StackSpacing) -> Component {
