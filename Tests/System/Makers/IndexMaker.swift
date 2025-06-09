1ab7453d2be05082793477cf4329cb857583a7e0
Cristian Rojas <cristian.rojas@live.fr>
2025-06-09 13:04:39 +0200

    Format

--- Tests/System/Makers/IndexMaker.swift
+++ Tests/System/Makers/IndexMaker.swift
@@ -1,22 +1,22 @@
 // © 2025  Cristian Felipe Patiño Rojas. Created on 9/6/25.
 
 struct IndexMaker {
-        protocol Provider {
-            func notes() throws -> [Note]
-            func tags() throws -> [Tag]
-        }
-        
-        protocol Renderer {
-            func render(notes: [Note], tags: [Tag]) -> String
-        }
-        
-        let provider: Provider
-        let renderer: Renderer
-        
-        func make() throws -> Resource {
-            let notes = try provider.notes()
-            let tags = try provider.tags()
-            let rendered = renderer.render(notes: notes, tags: tags)
-            return Resource(filename: "index.html", contents: rendered)
-        }
+    protocol Provider {
+        func notes() throws -> [Note]
+        func tags() throws -> [Tag]
     }
+    
+    protocol Renderer {
+        func render(notes: [Note], tags: [Tag]) -> String
+    }
+    
+    let provider: Provider
+    let renderer: Renderer
+    
+    func make() throws -> Resource {
+        let notes = try provider.notes()
+        let tags = try provider.tags()
+        let rendered = renderer.render(notes: notes, tags: tags)
+        return Resource(filename: "index.html", contents: rendered)
+    }
+}
