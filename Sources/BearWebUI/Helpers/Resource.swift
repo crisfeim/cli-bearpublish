8df031b96259c681a79d524b139b149c496483e8
Cristian Rojas <cristian.rojas@live.fr>
2025-06-09 20:21:58 +0200

    Remove warning as there is no more duplication of makeHash functions

--- Sources/BearWebUI/Helpers/Resource.swift
+++ Sources/BearWebUI/Helpers/Resource.swift
@@ -27,8 +27,6 @@ public struct Resource {
     }
 }
 
-
-#warning("There's already a hash method. See Core.SSG file")
 private extension String {
     func makeHash(maxLength: Int = 20) -> String {
         hash.description.replacingOccurrences(of: "-", with: "").prefix(maxLength) + ""
