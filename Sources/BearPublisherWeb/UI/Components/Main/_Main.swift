51936d013c91d55d57fa5f25602036a8913b43f7
Cristian Rojas <cristian.rojas@live.fr>
2025-06-09 15:00:31 +0200

    move components to folder

---
+++ Sources/BearPublisherWeb/UI/Components/Main/_Main.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cristian Felipe Patiño Rojas on 08/09/2023.
+//
+
+import Plot
+
+public struct Main: Component {
+    
+    private let content: String?
+    
+    public init(content: String? = nil) {
+        self.content = content
+    }
+    
+    @ComponentBuilder
+    public var body: Component {
+        Header {
+            Label("") {
+                SVG.chevron.render().makeRawNode()
+            }
+            .id("nav-toggle")
+            .attribute(named: "for", value: "nav-checkbox")
+            .hyperScript("on click set #menu-checkbox.checked to false")
+            .class("js-element")
+        }
+        
+        Article {
+            NoteDetailSkeleton()
+            if let content = content {
+                Div {
+                    Raw(text: content)
+                }
+                .class("content main-indicator")
+            }
+        }
+        .class("document-wrapper")
+    }
+    
+    var threedots: Component {
+        HStack {
+            Div {
+                SVG.threedots.render().makeRawNode()
+            }
+            .id("share-button")
+            .class("js-element")
+        }
+        .attribute(named: "spacing", value: "xs")
+    }
+}
+
+struct NoteDetailSkeleton: Component {
+    
+    var body: Component {
+        Div {
+            Div {}.class("title-placeholder")
+            Div {}.class("text-placeholder")
+            Div {}.class("text-placeholder")
+            Div {}.class("text-placeholder")
+            Div {}.class("text-placeholder")
+            Div {}.class("text-placeholder")
+            Div {}.class("text-placeholder")
+        }
+        .class("skeleton main-indicator")
+        .style("padding-top: 24px")
+    }
+}
---
+++ Sources/BearPublisherWeb/UI/Components/NoteList/NoteListView.Cell.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cristian Felipe Patiño Rojas on 08/09/2023.
+//
+
+import Plot
+import BearPublisherDomain
+
+extension NoteList {
+    struct Cell: Component {
+        let note: Note
+        let isSelected: Bool
+        var tabindex: Int { isSelected ? 1 : 0}
+        var pushedUrl: String { "/?slug=\(note.slug)" }
+        
+        var body: Component {
+            ListItem {
+                Link(url: pushedUrl) {
+                    VStack {
+                        H4(note.title)
+                            .class("title")
+                        
+                        if note.isEncrypted {
+                            EncryptedPlaceholder()
+                        }  else {
+                            Paragraph(note.subtitle)
+                                .class("subtitle")
+                        }
+                        
+                        HStack {
+                            if note.isPinned {
+                                SVG.pin.render().makeRawNode()
+                            }
+                            
+                            Time(datetime: "") {
+                                note.dateTime.makeRawNode()
+                            }
+                        }
+                        .attribute(named: "spacing", value: "xs")
+                        .style("padding-top: 16px")
+                    }
+                }
+                .hxGet("/standalone/note/\(note.slug)")
+                .hxTarget("main")
+                .hxPushUrl(pushedUrl)
+                .hxSwap("innerHTML scroll:top")
+                .hxIndicator(.class("main-indicator"))
+                .hyperScript("on click take .selected then Layout.toggleNav()")
+                .tabIndex(tabindex)
+                .class(isSelected ? "selected" : "")
+            }
+        }
+    }
+}
+
+enum Indicator {
+    case `class`(String)
+    case id(String)
+    
+    var value: String {
+        switch self {
+        case .class(let value): return ".\(value)"
+        case .id(let value): return "#\(value)"
+        }
+    }
+}
+
+extension NoteList {
+    struct EncryptedPlaceholder: Component {
+        var body: Component {
+            Div {
+                Div().class("encrypted-placeholder")
+                Div().class("encrypted-placeholder")
+            }
+        }
+    }
+}
