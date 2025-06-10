6166d589b3c45544a8409937ecf72d1e586988e2
Cristian Rojas <cristian.rojas@live.fr>
2025-06-10 18:56:23 +0200

    Guarantee site is built correctly

--- Tests/ComposerTests.swift
+++ Tests/ComposerTests.swift
@@ -8,7 +8,7 @@ class ComposerTests: XCTestCase {
     private var dbPath: String {
         Bundle.module.url(forResource: "database", withExtension: "sqlite")!.path
     }
-
+    
     func test() throws {
         let sut = try makeSite(dbPath: dbPath, outputURL: testSpecificURL())
         
@@ -50,11 +50,66 @@ class ComposerTests: XCTestCase {
         let sut = try makeSite(dbPath: dbPath, outputURL: testSpecificURL())
         try await sut.build()
         
-        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent("standalone/note").path))
-        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent("standalone/list").path))
-        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent("standalone/tag").path))
-        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent("assets/css").path))
-        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent("assets/js").path))
+        expectFileAtPathToExist("index.html")
+        
+        expectFileAtPathToExist("standalone/note/archived-note.html")
+        expectFileAtPathToExist("standalone/note/encrypted-note.html")
+        expectFileAtPathToExist("standalone/note/note-with-done-tasks.html")
+        expectFileAtPathToExist("standalone/note/note-with-file.html")
+        expectFileAtPathToExist("standalone/note/note-with-source-code.html")
+        expectFileAtPathToExist("standalone/note/note-with-tag.html")
+        expectFileAtPathToExist("standalone/note/note-with-undone-tasks.html")
+        expectFileAtPathToExist("standalone/note/pinned-note.html")
+        expectFileAtPathToExist("standalone/note/regular-note.html")
+        expectFileAtPathToExist("standalone/note/trashed-note.html")
+        
+        expectFileAtPathToExist("standalone/list/all.html")
+        expectFileAtPathToExist("standalone/list/archived.html")
+        expectFileAtPathToExist("standalone/list/tasks.html")
+        expectFileAtPathToExist("standalone/list/trashed.html")
+        
+        expectFileAtPathToExist("standalone/tag/code.html")
+        expectFileAtPathToExist("standalone/tag/dev.html")
+        expectFileAtPathToExist("standalone/tag/sometag.html")
+        expectFileAtPathToExist("standalone/tag/sometag&nested.html")
+        expectFileAtPathToExist("standalone/tag/sometag&nested&nested.html")
+        
+        expectFileAtPathToExist("assets/css")
+        
+        
+        try expectFileWithHashToExist(parentFolder: "assets/css", fileNamePrefix: "theme-variables")
+        try expectFileWithHashToExist(parentFolder: "assets/css", fileNamePrefix: "styles")
+        
+        try expectFileWithHashToExist(parentFolder: "assets/js", fileNamePrefix: "htmx")
+        try expectFileWithHashToExist(parentFolder: "assets/js", fileNamePrefix: "hyperscript")
+        try expectFileWithHashToExist(parentFolder: "assets/js", fileNamePrefix: "main")
+        
+        expectFileAtPathToExist("assets/js")
+    }
+    
+}
+
+// MARK: - Custom asserts
+private extension ComposerTests {
+    
+    func expectFileWithHashToExist(parentFolder: String, fileNamePrefix: String, file: StaticString = #filePath, line: UInt = #line) throws {
+        
+        let directoryURL = testSpecificURL().appendingPathComponent(parentFolder)
+        
+        let contents = try FileManager.default.contentsOfDirectory(
+            at: directoryURL,
+            includingPropertiesForKeys: nil
+        )
+        
+        let cssFile = contents.first {
+            $0.lastPathComponent.hasPrefix(fileNamePrefix)
+        }
+        
+        XCTAssertNotNil(cssFile, "Expected to find a '\(fileNamePrefix)-<hash>' file in \(directoryURL.path)")
+    }
+    
+    func expectFileAtPathToExist(_ path: String, file: StaticString = #filePath, line: UInt = #line) {
+        XCTAssert(FileManager.default.fileExists(atPath: testSpecificURL().appendingPathComponent(path).path))
     }
 }
 
@@ -68,6 +123,7 @@ func .*<T>(lhs: T, rhs: (inout T) -> Void) -> T {
   return copy
 }
 
+// MARK: - Helpers
 private extension ComposerTests {
     func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
