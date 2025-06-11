47c694572b6613776235fb6f24b56833e5162eee
Cristian Rojas <cristian.rojas@live.fr>
2025-06-11 13:46:41 +0200

    Move file mapper to its own file

---
+++ Sources/BearPublish/Mappers/FileMapper.swift
@@ -0,0 +1,15 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 11/6/25.
+
+import BearDatabase
+import BearDomain
+
+enum FileMapper {
+    static func map(_ file: BearDatabase.File) -> BearDomain.File {
+        File(
+            id: file.id,
+            name: file.name,
+            extension: file.extension,
+            size: file.size
+        )
+    }
+}
