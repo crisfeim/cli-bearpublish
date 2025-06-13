ceb4bf2b1460f65776a5dce40385af7462c3f176
Cristian Rojas <cristian.rojas@live.fr>
2025-06-13 11:13:50 +0200

    Builder.execute delivers error on list by category provider

--- Tests/BearSiteBuilderTests.swift
+++ Tests/BearSiteBuilderTests.swift
@@ -7,14 +7,23 @@ class BearSiteBuilderTests: XCTestCase {
     
     struct BearSiteBuilder {
         let notesProvider: () throws -> [Note]
+        let listsByCategoryProvider: () throws -> [NoteList]
         func execute() throws {
            _ = try notesProvider()
+           _ = try listsByCategoryProvider()
         }
     }
     
     func test_execute_deliversErrorOnNotesProviderError() throws {
-        
         let sut = BearSiteBuilder(notesProvider: {
+            throw NSError(domain: "any error", code: 0)
+        }, listsByCategoryProvider: {[]})
+        
+        XCTAssertThrowsError(try sut.execute())
+    }
+    
+    func test_execute_deliversErrorOnListsByCategoryProviderError() throws {
+        let sut = BearSiteBuilder(notesProvider: {[]}, listsByCategoryProvider: {
             throw NSError(domain: "any error", code: 0)
         })
         
