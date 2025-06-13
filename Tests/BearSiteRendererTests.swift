6d08cf214ea05bb05adf7d9cda91a0b485cd2b90
Cristian Rojas <cristian.rojas@live.fr>
2025-06-13 12:17:35 +0200

    Renderer.execute passes correct arguments to note renderer

--- Tests/BearSiteRendererTests.swift
+++ Tests/BearSiteRendererTests.swift
@@ -8,6 +8,7 @@ class BearSiteRendererTests: XCTestCase {
     
     struct BearRenderedSite {
         let index: Resource
+        let notes: [Resource]
     }
     
     struct BearSiteRenderer {
@@ -16,8 +17,13 @@ class BearSiteRendererTests: XCTestCase {
             func render(title: String, lists: [NoteList], notes: [Note], tags: [Tag]) -> Resource
         }
         
+        protocol NoteRenderer {
+            func render(title: String, slug: String, content: String) -> Resource
+        }
+        
         let site: BearSite
         let indexRenderer: IndexRenderer
+        let noteRenderer: NoteRenderer
         
         func execute() -> BearRenderedSite {
             BearRenderedSite(
@@ -26,7 +32,16 @@ class BearSiteRendererTests: XCTestCase {
                     lists: site.listsByCategory,
                     notes: site.notes,
                     tags: site.tags
-                )
+                ),
+                notes: site.notes.map(renderNote)
+            )
+        }
+        
+        func renderNote(_ note: Note) -> Resource {
+            noteRenderer.render(
+                title: note.title,
+                slug: note.slug,
+                content: note.content
             )
         }
     }
@@ -34,7 +49,11 @@ class BearSiteRendererTests: XCTestCase {
     func test_execute_passesCorrectArgumentsToIndexRenderer() throws {
         let renderer = IndexRendererSpy()
         let site = anyBearSite()
-        let sut = BearSiteRenderer(site: site, indexRenderer: renderer)
+        let sut = BearSiteRenderer(
+            site: site,
+            indexRenderer: renderer,
+            noteRenderer: NoteRendererDummy()
+        )
         let _ = sut.execute()
         
         XCTAssertEqual(renderer.capturedTitle, site.title)
@@ -47,25 +66,45 @@ class BearSiteRendererTests: XCTestCase {
         let stub = Resource(filename: "index.html", contents: "index contents")
         let renderer = IndexRendererStub(stub: stub)
         let site = anyBearSite()
-        let sut = BearSiteRenderer(site: site, indexRenderer: renderer)
+        let sut = BearSiteRenderer(
+            site: site,
+            indexRenderer: renderer,
+            noteRenderer: NoteRendererDummy()
+        )
         let rendered = sut.execute()
         
         XCTAssertEqual(rendered.index, stub)
     }
+    
+    func test_execute_passCorrectArgumentsToNoteRenderer() throws {
+        let renderer = NoteRendererSpy()
+        let stubbedNotes = [anyNote(), anyNote(), anyNote(), anyNote()]
+        let site = anyBearSite(notes: stubbedNotes)
+        let sut = BearSiteRenderer(
+            site: site,
+            indexRenderer: IndexRendererDummy(),
+            noteRenderer: renderer
+        )
+        _ = sut.execute()
+        
+        XCTAssertEqual(renderer.capturedNotes, stubbedNotes.map {
+            NoteRendererSpy.Captured(title: $0.title, slug: $0.slug, content: $0.content)
+        })
+    }
 }
 
 private extension BearSiteRendererTests {
-    func anyBearSite() -> BearSite {
+    func anyBearSite(notes: [Note] = [anyNote()]) -> BearSite {
         BearSite(
             title: "title",
-            notes: [anyNote(), anyNote()],
+            notes: notes,
             tags: [anyTag()],
             listsByCategory: [anyNoteList(), anyNoteList()],
             listsByTag: [anyNoteList()]
         )
     }
     
-    func anyNote() -> Note {
+    static func anyNote() -> Note {
         Note(
             id: 0,
             title: "any note",
@@ -80,12 +119,22 @@ private extension BearSiteRendererTests {
         )
     }
     
+    func anyNote() -> Note {
+        Self.anyNote()
+    }
+    
     func anyNoteList() -> NoteList {
         NoteList(title: "any note list", slug: "any-note-list", notes: [anyNote()])
     }
     
     func anyTag() -> Tag {
         Tag(name: "any tag", fullPath: "any-tag", notesCount: 0, children: [], isPinned: false)
+    }
+    
+    struct IndexRendererDummy: BearSiteRenderer.IndexRenderer {
+        func render(title: String, lists: [NoteList], notes: [Note], tags: [Tag]) -> Resource {
+            Resource(filename: "", contents: "")
+        }
     }
     
     struct IndexRendererStub: BearSiteRenderer.IndexRenderer {
@@ -108,6 +157,26 @@ private extension BearSiteRendererTests {
             capturedNotes = notes
             capturedTags = tags
             return Resource(filename: "", contents: "")
+        }
+    }
+    
+    class NoteRendererSpy: BearSiteRenderer.NoteRenderer {
+        struct Captured: Equatable {
+            let title: String
+            let slug: String
+            let content: String
+        }
+        
+        var capturedNotes = [Captured]()
+        func render(title: String, slug: String, content: String) -> Resource {
+            capturedNotes.append(Captured(title: title, slug: slug, content: content))
+            return  Resource(filename: "", contents: "")
+        }
+    }
+    
+    struct NoteRendererDummy: BearSiteRenderer.NoteRenderer {
+        func render(title: String, slug: String, content: String) -> Resource {
+            Resource(filename: "", contents: "")
         }
     }
 }
